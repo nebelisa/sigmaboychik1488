16e27e99ccf2c8d148fd311a034c9c413e090f61
nebelisa <75169237+nebelisa@users.noreply.github.com>
2025-02-02 16:26:48 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 # wsgi.py
-from app import app  # Импортируем ваше Flask-приложение
+from pypypy import pypypy  # Импортируем ваше Flask-приложение
 
 if __name__ == "__main__":
-    app.run()
+    pypypy.run()
